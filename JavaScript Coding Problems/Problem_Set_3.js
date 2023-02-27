913974406ec0d3ff144db1f382114bc63f7b4bff
mamidishettikarthik <124386275+mamidishettikarthik@users.noreply.github.com>
2023-02-27 10:54:39 +0530

    comment added

--- JavaScript Coding Problems/Problem_Set_3.js
+++ JavaScript Coding Problems/Problem_Set_3.js
@@ -2,14 +2,14 @@
 //                             -----Prefill an Array--------
 
 
-// Create the function prefill that returns an array of n elements that all have the same value v. See if you can do this without using a loop.
+/* Create the function prefill that returns an array of n elements that all have the same value v. See if you can do this without using a loop.
 
-// You have to validate input:
+ You have to validate input:
 
-// v can be anything (primitive or otherwise)
-// if v is ommited, fill the array with undefined
-// if n is 0, return an empty array
-// if n is anything other than an integer or integer-formatted string (e.g. '123') that is >=0, throw a TypeError
+ v can be anything (primitive or otherwise)
+ if v is ommited, fill the array with undefined
+ if n is 0, return an empty array
+ if n is anything other than an integer or integer-formatted string (e.g. '123') that is >=0, throw a TypeError  */
 
 
 function prefill(n, v) {
@@ -21,12 +21,12 @@ function prefill(n, v) {
 // input:prefill(3,1)
 // output: [1,1,1]
 
-// ----------------------------------------Closures and Scopes----------------------
+/* ----------------------------------------Closures and Scopes----------------------
 
-// We want to create a function, which returns an array of functions, which return their index in the array. For better understanding, here an example:
-// var callbacks = createFunctions(5); // create an array, containing 5 functions
-// callbacks[0](); // must return 0
-// callbacks[3](); // must return 3
+ We want to create a function, which returns an array of functions, which return their index in the array. For better understanding, here an example:
+ var callbacks = createFunctions(5); // create an array, containing 5 functions
+ callbacks[0](); // must return 0
+ callbacks[3](); // must return 3        */
 
 function createFunctions(n) {
 
@@ -37,21 +37,21 @@ function createFunctions(n) {
   }
   return callbacks;
 }
-input:3
-output:3
+//input:3
+//output:3
 
-// ---------------------------Can you keep a secret?---------------------------------
+/* ---------------------------Can you keep a secret?---------------------------------
 
-// There's no such thing as private properties on a coffeescript object! But, maybe there are?
+ There's no such thing as private properties on a coffeescript object! But, maybe there are?
 
-// Implement a function createSecretHolder(secret) which accepts any value as secret and returns an object with ONLY two methods
+ Implement a function createSecretHolder(secret) which accepts any value as secret and returns an object with ONLY two methods
 
-// getSecret() which returns the secret
-// setSecret() which sets the secret
-// obj = createSecretHolder(5)
-// obj.getSecret() # returns 5
-// obj.setSecret(2)
-// obj.getSecret() # returns 2
+ getSecret() which returns the secret
+ setSecret() which sets the secret
+ obj = createSecretHolder(5)
+ obj.getSecret() # returns 5
+ obj.setSecret(2)
+ obj.getSecret() # returns 2   */
 
 function createSecretHolder(secret) {
   
